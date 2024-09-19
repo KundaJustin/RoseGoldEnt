72ba38e3e29bb30e5b6f11abffaac8d88e535979
Mwapsam <mwapsam@gmail.com>
2024-09-19 12:41:02 +0200

    Initial commit

--- rosegold_v1/settings/base.py
+++ rosegold_v1/settings/base.py
@@ -1,7 +1,13 @@
 import os
+import environ
+import dj_database_url
+
+env = environ.Env()
 
 PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 BASE_DIR = os.path.dirname(PROJECT_DIR)
+
+environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
 
 INSTALLED_APPS = [
     "home",
@@ -62,11 +68,17 @@ TEMPLATES = [
 WSGI_APPLICATION = "rosegold_v1.wsgi.application"
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
-    }
+    "default": dj_database_url.parse(
+        os.environ.get(
+            "POSTGRES_URL",
+            "sqlite:///db.sqlite3",
+        ),
+        conn_max_age=600,
+        conn_health_checks=True,
+    )
 }
+
+print("POSTGRES_URL:", os.environ.get("POSTGRES_URL"))
 
 AUTH_PASSWORD_VALIDATORS = [
     {
