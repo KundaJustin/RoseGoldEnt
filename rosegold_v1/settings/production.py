a1cb01bc000418d81f607ac17daf01f9eb6bdfb4
Mwapsam <mwapsam@gmail.com>
2024-11-12 06:34:10 +0200

    Deployment

--- rosegold_v1/settings/production.py
+++ rosegold_v1/settings/production.py
@@ -1,12 +1,7 @@
 from .base import *
 
-DEBUG = True
+DEBUG = False
 SECRET_KEY = "django-insecure-pm^-sh-=6#kz(_ux(c*%bbri8hto_ihp5=ecp!8bf54+9x46yo"
-ALLOWED_HOSTS = [".vercel.app", "localhost"]
+ALLOWED_HOSTS = [".vercel.app", "localhost", ".rosegold-ent.com"]
 
 
-DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
-AWS_ACCESS_KEY_ID = "your-cloudflare-access-key-id"
-AWS_SECRET_ACCESS_KEY = "your-cloudflare-secret-access-key"
-AWS_STORAGE_BUCKET_NAME = "your-r2-namespace"
-AWS_S3_ENDPOINT_URL = "<https://storage.developers.cloudflare.com>"
