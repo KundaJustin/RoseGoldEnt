2ac22996ba0d10538bd81f30480272f78f3e113f
Mwapsam <mwapsam@gmail.com>
2024-11-14 12:20:15 +0200

    Deployment

---
+++ docker/postgres/init.sql
@@ -0,0 +1,5 @@
+CREATE USER rosegold_api WITH PASSWORD 'Suguadsgyugyuqwgy90wqugbhjfghfw';
+
+CREATE DATABASE rosegold_api_db OWNER rosegold_api;
+
+ALTER DATABASE rosegold_api_db CONNECTION LIMIT -1;
